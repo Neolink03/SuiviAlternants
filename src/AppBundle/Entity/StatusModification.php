82c4a67ee061ad7dac1583e40500206e6ac69ede
Antoine Lamirault <antoine.lamirault@etu.univ-lyon1.fr>
2017-03-31 11:14:15 +0200

    Add Status modification entity and fixtures

---
+++ src/AppBundle/Entity/StatusModification.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace AppBundle\Entity;
+
+/**
+ * StatusModification
+ */
+class StatusModification
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var \DateTime
+     */
+    private $dateTime;
+
+    /**
+     * @var string
+     */
+    private $comment;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set dateTime
+     *
+     * @param \DateTime $dateTime
+     *
+     * @return StatusModification
+     */
+    public function setDateTime($dateTime)
+    {
+        $this->dateTime = $dateTime;
+
+        return $this;
+    }
+
+    /**
+     * Get dateTime
+     *
+     * @return \DateTime
+     */
+    public function getDateTime()
+    {
+        return $this->dateTime;
+    }
+
+    /**
+     * Set comment
+     *
+     * @param string $comment
+     *
+     * @return StatusModification
+     */
+    public function setComment($comment)
+    {
+        $this->comment = $comment;
+
+        return $this;
+    }
+
+    /**
+     * Get comment
+     *
+     * @return string
+     */
+    public function getComment()
+    {
+        return $this->comment;
+    }
+}
+
