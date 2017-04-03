9eb34e1c7b17b7c796381c985f2b37f887e944cf
jeremiebernard <jeremie71000@hotmail.fr>
2017-04-03 11:09:40 +0200

    EditCourse

--- src/AppBundle/Controller/CourseManagerController.php
+++ src/AppBundle/Controller/CourseManagerController.php
@@ -10,9 +10,9 @@ namespace AppBundle\Controller;
 
 
 use AppBundle\Entity\Course;
-use AppBundle\Entity\Promotion;
 use AppBundle\Entity\User\Student;
 use AppBundle\Forms\Types\AddPromotionType;
+use AppBundle\Forms\Types\Courses\EditCourseType;
 use AppBundle\Forms\Types\UserType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -53,22 +53,29 @@ class CourseManagerController extends Controller
     public function editCourseAction(Request $request)
     {
         $em = $this->getDoctrine()->getManager();
+        $course = $em->getRepository(Course::class)->find($request->get('courseId'));
+        if (!$course) {
+            $this->addFlash('danger', 'La formation est introuvable.');
+            // return $this->redirectToRoute(''); Redirect to CourseList
+        }
 
-        $promotionForm = $this->createForm(AddPromotionType::class);
+        $editCourseForm = $this->createForm(EditCourseType::class, $course);
+        $addPromotionForm = $this->createForm(AddPromotionType::class);
 
         if ($request->isMethod('post')) {
 
-            /*$promotionForm->handleRequest($request);
+            $editCourseForm->handleRequest($request);
 
-            if ($promotionForm->isSubmitted() && $promotionForm->isValid()) {
-                $em->persist($promotion);
+            if ($editCourseForm->isSubmitted() && $editCourseForm->isValid()) {
+                $em->persist($course);
                 $em->flush();
-                $this->addFlash('success', 'La promotion a été ajoutée avec succès.');
-            }*/
+                $this->addFlash('success', 'La formation a été modifiée avec succès.');
+            }
         }
 
         return $this->render('AppBundle:CourseManager:editCourse.html.twig', [
-            'promotionForm' => $promotionForm->createView()
+            'editCourseForm' => $editCourseForm->createView(),
+            'addPromotionForm' => $addPromotionForm->createView()
         ]);
     }
 
@@ -81,7 +88,7 @@ class CourseManagerController extends Controller
 
         $this->addFlash('success', 'La promotion a été ajoutée avec succès.');
 
-        return $this->redirectToRoute('course_manager.course.edit', ['courseId' => $courseId]);
+        return $this->redirectToRoute('course_manager.course.edit', ['courseId' => $courseId]); // Redirect to CourseList
     }
 
 }
