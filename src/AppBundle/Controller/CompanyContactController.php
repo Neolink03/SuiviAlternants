11b582e307ed9d8f4538dd3c84c1dbbc6ca79c00
Neolink03 <link.03@hotmail.fr>
2017-04-05 15:57:31 +0200

    Personnalisation edit company contact
    * style
    * remove delete button

--- src/AppBundle/Controller/CompanyContactController.php
+++ src/AppBundle/Controller/CompanyContactController.php
@@ -71,7 +71,6 @@ class CompanyContactController extends Controller
      */
     public function editAction(Request $request, CompanyContact $companyContact, Course $course)
     {
-        $deleteForm = $this->createDeleteForm($course, $companyContact);
         $editForm = $this->createForm(CompanyContactType::class, $companyContact);
         $editForm->handleRequest($request);
 
@@ -87,7 +86,6 @@ class CompanyContactController extends Controller
         return $this->render('@App/companycontact/edit.html.twig', array(
             'companyContact' => $companyContact,
             'edit_form' => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
             'course' => $course,
         ));
     }
