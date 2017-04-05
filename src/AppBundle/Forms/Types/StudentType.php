d94e693e21f834abb6cb453450ceae17a47453be
Vic4 <victor.anglada9@gmail.com>
2017-04-05 14:34:10 +0200

    Add regex

--- src/AppBundle/Forms/Types/StudentType.php
+++ src/AppBundle/Forms/Types/StudentType.php
@@ -14,6 +14,7 @@ use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Regex;
 
 class StudentType extends AbstractType
 {
