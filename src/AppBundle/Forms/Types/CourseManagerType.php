85a109f42fde10a831083596c3caf059a89eb6d6
Vic4 <victor.anglada9@gmail.com>
2017-05-12 15:38:06 +0200

    modification of format of phone

--- src/AppBundle/Forms/Types/CourseManagerType.php
+++ src/AppBundle/Forms/Types/CourseManagerType.php
@@ -24,8 +24,8 @@ class CourseManagerType extends AbstractType
                 'required' => false,
                 'constraints' => array(new Regex(
                     array(
-                        'pattern' => "/^((0|\\(\\+33\\))[0-9]{9})?$/",
-                        'message' => 'Le numéro de téléphone doit être de format 0xxxxxxxxx ou (+33)xxxxxxxxx',)
+                        'pattern' => "/^((0|\\+33)[0-9]{9})?$/",
+                        'message' => 'Le numéro de téléphone doit être de format 0xxxxxxxxx ou +33xxxxxxxxx',)
                 ))
             ));
     }
