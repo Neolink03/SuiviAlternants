ddd44993b47ecf891b40cfbdfa271c4fad45dc6c
Vic4 <victor.anglada9@gmail.com>
2017-05-12 15:15:31 +0200

    jury + small corrections

--- src/AppBundle/Controller/CourseManagerController.php
+++ src/AppBundle/Controller/CourseManagerController.php
@@ -13,10 +13,11 @@ use AppBundle\Entity\Application;
 use AppBundle\Entity\Course;
 use AppBundle\Entity\Promotion;
 use AppBundle\Entity\State;
+use AppBundle\Entity\User\Jury;
 use AppBundle\Entity\User\Student;
+use AppBundle\Forms\Types\AddJuryType;
 use AppBundle\Forms\Types\AddPromotionType;
 use AppBundle\Forms\Types\Applications\ChangeStatusType;
-use AppBundle\Forms\Types\CourseManagerType;
 use AppBundle\Forms\Types\Courses\CourseType;
 use AppBundle\Forms\Types\EmailMessageType;
 use AppBundle\Forms\Types\PromotionFormType;
@@ -27,8 +28,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
-use Symfony\Component\HttpFoundation\Response;
-use AppBundle\Models\Dtos\Courses\Course as CourseDto;
 
 
 class CourseManagerController extends Controller
@@ -302,5 +301,36 @@ class CourseManagerController extends Controller
             'form' => $form->createView()
         ]);
     }
-    
+
+    /**
+     * @ParamConverter("course", options={"mapping": {"courseId" : "id"}})
+     */
+    public function addJuryAction(Request $request, Course $course){
+
+        $em = $this->getDoctrine()->getManager();
+
+        $form = $this->createForm(AddJuryType::class, null, array(
+            'applications' => $jury = $this->getDoctrine()->getManager()->getRepository(Jury::class)->findAll()
+        ));
+
+        if ($request->isMethod('post')) {
+
+            $form->handleRequest($request);
+
+            if ($form->isSubmitted() && $form->isValid()) {
+
+                $data = $form->getData();
+
+                $course->setJurys($data['jurys']);
+                $em->persist($course);
+                $em->flush();
+                $this->addFlash("success", "Les jurys ont bien été ajoutés à la promotion");
+            }
+        }
+
+        return $this->render('AppBundle:CourseManager:addJuryToCourse.html.twig',[
+            'juryList' => $form->createView(),
+            'course' => $course
+        ]);
+    }
 }
