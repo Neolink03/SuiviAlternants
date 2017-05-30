362a951c5fe0614f3a2cd8d6f6a614d6efb5fd5d
jeremiebernard <jeremie71000@hotmail.fr>
2017-05-30 14:22:24 +0200

    zoom in workflow

--- web/js/workflowDisplay.js
+++ web/js/workflowDisplay.js
@@ -3,17 +3,19 @@
  */
 $(function () {
     var workflows = $('.workflow-dump');
+
     workflows.each(function (index) {
         var result = Viz(this.innerText, {format: "png-image-element", scale: 1});
 
         result.style.maxWidth = '100%';
         result.style.height = 'auto';
-        result.style.maxHeight = '400px';
 
         this.removeChild(this.firstChild);
         this.appendChild(result);
         this.style.display = 'block';
     });
+
+    $("img:first", workflows).attr("data-action", "zoom");
 
     $('[data-toggle="tooltip"]').tooltip();
 });
